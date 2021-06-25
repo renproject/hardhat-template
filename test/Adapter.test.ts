8e8bb6cb74a58568e1329055c9645c7aea9552cb
noiach <gh@noiach.com>
2021-06-25 11:34:21 +1000

    feat: tidied up test initialization

--- test/Adapter.test.ts
+++ test/Adapter.test.ts
@@ -1,7 +1,7 @@
 import { ethers } from "hardhat";
 import chai from "chai";
 import { solidity } from "ethereum-waffle";
-import { Adapter__factory, GatewayFactory__factory, BasicAdapter__factory } from "../typechain";
+import { Adapter__factory, GatewayFactory__factory, BasicAdapter__factory, Adapter } from "../typechain";
 import BigNumber from "bignumber.js";
 
 // RenJS imports
@@ -9,84 +9,81 @@ import { MockProvider, MockChain } from "@renproject/mock-provider";
 import RenJS from "@renproject/ren";
 import { RenVMProvider } from "@renproject/rpc/build/main/v2";
 import { Ethereum, EthereumConfig } from "@renproject/chains-ethereum";
-import { Ox } from "@renproject/utils";
 
 chai.use(solidity);
 const { expect } = chai;
 
 describe("Token", () => {
-  let adapterAddress: string;
-  let gatewayRegistryAddress: string;
+  let adapter: Adapter;
 
-  let bitcoin: MockChain;
+  let Bitcoin: MockChain;
   let renJS: RenJS;
   let network: EthereumConfig;
 
   beforeEach(async () => {
     const [deployer, user] = await ethers.getSigners();
 
+    ////////////////////////////////////////////////////////////////////////////
+    // RenJS set-up ////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
+
     const mockRenVMProvider = new MockProvider();
     renJS = new RenJS(new RenVMProvider("testnet", mockRenVMProvider));
 
     // Set up mock Bitcoin chain.
-    bitcoin = new MockChain();
-    mockRenVMProvider.registerChain(bitcoin);
+    Bitcoin = new MockChain();
+    mockRenVMProvider.registerChain(Bitcoin);
 
     // Get mint authority from mock provider.
     const mintAuthority = mockRenVMProvider.mintAuthority();
 
     // Deploy Gateway Factory.
-    const gatewayFactoryFactory = new GatewayFactory__factory(deployer);
-    const gatewayFactory = await gatewayFactoryFactory.deploy(mintAuthority, "Ethereum");
-    gatewayRegistryAddress = Ox(await await gatewayFactory.registry());
+    const gatewayFactory = await new GatewayFactory__factory(deployer).deploy(mintAuthority, "Ethereum");
+    const gatewayRegistryAddress = await await gatewayFactory.registry();
 
-    // Deploy BTC token and gateway.
+    // Deploy BTC and ZEC tokens and gateways.
     await gatewayFactory.addToken("Bitcoin", "BTC", 8);
-
-    // Deploy ZEC token and gateway.
     await gatewayFactory.addToken("Zcash", "ZEC", 8);
 
     // Deploy BasicAdapter.
-    const basicAdapterFactory = new BasicAdapter__factory(deployer);
-    const basicAdapter = await basicAdapterFactory.deploy(gatewayRegistryAddress);
+    const basicAdapter = await new BasicAdapter__factory(deployer).deploy(gatewayRegistryAddress);
 
-    network = {
-      name: "hardhat",
-      chain: "hardhat",
-      isTestnet: true,
-      networkID: (await user.provider?.getNetwork())?.chainId!,
-      chainLabel: "Hardhat",
-      infura: "",
-      etherscan: "",
-      addresses: {
-        GatewayRegistry: gatewayRegistryAddress,
-        BasicAdapter: basicAdapter.address,
-      },
-    };
+    // Set up Ethereum network config.
+    const providerNetwork = await user.provider?.getNetwork();
+    const networkID = providerNetwork ? providerNetwork.chainId : 0;
+    network = LocalEthereumNetwork(networkID, gatewayRegistryAddress, basicAdapter.address);
+
+    ////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
 
     // Deploy Adapter.
-    const adapterFactory = new Adapter__factory(deployer);
-    const adapterContract = await adapterFactory.deploy(gatewayRegistryAddress);
-    adapterAddress = adapterContract.address;
+    adapter = await new Adapter__factory(deployer).deploy(gatewayRegistryAddress);
   });
 
   describe("Mint", async () => {
     it("Should mint some tokens", async () => {
       const [_, user] = await ethers.getSigners();
 
-      const amount = new BigNumber(0.003).times(
-        new BigNumber(10).exponentiatedBy(bitcoin.assetDecimals(bitcoin.asset)),
-      );
+      // Use random amount, multiplied by the asset's decimals.
+      const amount = new BigNumber(Math.random())
+        .times(new BigNumber(10).exponentiatedBy(Bitcoin.assetDecimals(Bitcoin.asset)))
+        .integerValue(BigNumber.ROUND_DOWN);
 
+      // MockProvider doesn't yet return fee details.
+      const fixedFee = 1000; // sats
+      const percentFee = 15; // BIPS
+
+      // Initialize RenJS lockAndMint.
       const mint = await renJS.lockAndMint({
         // Send BTC from the Bitcoin blockchain to the Ethereum blockchain.
-        asset: bitcoin.asset,
-        from: bitcoin,
+        asset: "BTC", // `bitcoin.asset`
+        from: Bitcoin,
         // If you change this to another chain, you also have to change the
         // chain name passed to `gatewayFactory` above.
         to: Ethereum(user.provider! as any, user, network).Contract({
           // The contract we want to interact with
-          sendTo: Ox(adapterAddress),
+          sendTo: adapter.address,
 
           // The name of the function we want to call
           contractFn: "deposit",
@@ -103,8 +100,11 @@ describe("Token", () => {
       });
 
       // Mock deposit.
-      bitcoin.addUTXO(mint.gatewayAddress!, amount.toNumber());
+      Bitcoin.addUTXO(mint.gatewayAddress!, amount.toNumber());
 
+      const balanceBefore = await adapter.balance();
+
+      // Process the deposit, including the mint step.
       await new Promise<void>((resolve, reject) => {
         mint.on("deposit", async deposit => {
           try {
@@ -117,6 +117,28 @@ describe("Token", () => {
           }
         });
       });
+
+      // Check that the balance of the contract increased by the expected amount.
+      const balanceAfter = await adapter.balance();
+      const expected = amount
+        .minus(fixedFee)
+        .times(1 - percentFee / 10000)
+        .integerValue(BigNumber.ROUND_UP);
+      expect(balanceAfter.sub(balanceBefore)).to.equal(expected.toFixed());
     });
   });
+});
+
+const LocalEthereumNetwork = (networkID: number, gatewayRegistry: string, basicAdapter: string) => ({
+  name: "hardhat",
+  chain: "hardhat",
+  chainLabel: "Hardhat",
+  isTestnet: true,
+  networkID,
+  infura: "",
+  etherscan: "",
+  addresses: {
+    GatewayRegistry: gatewayRegistry,
+    BasicAdapter: basicAdapter,
+  },
 });
